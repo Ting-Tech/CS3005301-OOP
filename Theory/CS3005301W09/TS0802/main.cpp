93cbbf1e44a0da6fb8d427397e3f4fbb3f6c153e
Ting <67228710+Ting-Tech@users.noreply.github.com>
2023-04-24 20:36:55 +0800

    week 9 start

---
+++ Theory/CS3005301W09/TS0802/main.cpp
@@ -0,0 +1,69 @@
+﻿/*
+Author: 鄭健廷(B11130225@mail.ntust.edu.tw)
+Date: April 24, 2023
+Last Update: April 24, 2023
+Describition:
+*/
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+	size_t rowA, colA, rowB, colB;
+	cin >> rowA >> colA >> rowB >> colB;
+	vector<vector<int>> matrixA(rowA,vector<int>(colA, 0));
+	vector<vector<int>> matrixB(rowB, vector<int>(colB, 0));
+	vector<vector<int>> result(rowA, vector<int>(colB, 0));
+
+	for (size_t i = 0; i < rowA; i++)
+	{
+		for (size_t j = 0; j < colA; j++)
+		{
+			int inputNum = 0;
+			cin >> inputNum;
+			matrixA[i][j] = inputNum;
+		}
+	}
+	for (size_t i = 0; i < rowB; i++)
+	{
+		for (size_t j = 0; j < colB; j++)
+		{
+			int inputNum = 0;
+			cin >> inputNum;
+			matrixB[i][j] = inputNum;
+		}
+	}
+
+	if (colA != rowB)
+	{
+		cout << "Matrix multiplication failed.";
+	}
+	else
+	{
+		for (size_t i = 0; i < rowA; i++)
+		{
+			for (size_t j = 0; j < colB; j++)
+			{
+				int sum = 0;
+				for (size_t k = 0; k < colA; k++)
+				{
+					sum += matrixA[i][k] * matrixB[k][j];
+				}
+				result[i][j] = sum;
+			}
+		}
+		for (size_t i = 0; i < rowA; i++)
+		{
+			for (size_t j = 0; j < colB; j++)
+			{
+				cout << result[i][j];
+				if (j < colB - 1)
+					cout << ' ';
+			}
+			cout << endl;
+		}
+	}
+}
---
+++ Theory/CS3005301W09/TS0803/main.cpp
@@ -0,0 +1,17 @@
+﻿/*
+Author: 鄭健廷(B11130225@mail.ntust.edu.tw)
+Date: April 24, 2023
+Last Update: April 24, 2023
+Describition: 
+*/
+
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+
+}
